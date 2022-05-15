101fc09deec100c3e6227132572524fb238182f8
mrserjo <mrserjo@mail.ru>
2022-05-15 20:27:51 +0300

    bs4 parser done

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import lxml
 
 
 url = 'https://www.amazon.com/Newest-Plays-TATION-Version-Gaming-Console/dp/B0B11GWBWG/'
+url2 = 'https://www.amazon.com/HP-24mh-FHD-Monitor-Built/dp/B08BF4CZSV'
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
-    "Accept-Language": "en",
-}
 
-r = requests.get(url, headers=headers)
+def get_link_data(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
+        "Accept-Language": "en",
+    }
 
-soup = BeautifulSoup(r.text, "lxml")
+    r = requests.get(url, headers=headers)
 
-name = soup.find('span', class_='a-offscreen').text
-print(name)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    name = soup.select_one(selector="#productTitle").getText()
+    name = name.strip().replace('-', '')
+
+    price = soup.select_one(selector='#price_inside_buybox').getText()
+    price = price.strip()
+    price = float(price[1:])
+
+    return name, price
+
+print(get_link_data(url))
+print(get_link_data(url2))
