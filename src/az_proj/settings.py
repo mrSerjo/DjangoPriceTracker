ef8c02d0f6a8b8df6e619a84bfe0a3208a6d68dc
mrserjo <mrserjo@mail.ru>
2022-05-17 09:01:36 +0300

    bootstrap modal form added, crispy-forms added

--- src/az_proj/settings.py
+++ src/az_proj/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'links',
 ]
 
@@ -110,3 +111,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
